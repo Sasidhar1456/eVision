b413ba117c8e2dcee696145b9844289ff27a68f2
Hemasri-2312 <yhemasri2312@gmail.com>
2024-06-24 16:20:21 +0530

    hemasri

--- server/insert_emp/views.py
+++ server/insert_emp/views.py
@@ -2,11 +2,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Employee
 from .serializers import EmployeeSerializer
 
 class EmployeeListView(APIView):
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         serializer = EmployeeSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
